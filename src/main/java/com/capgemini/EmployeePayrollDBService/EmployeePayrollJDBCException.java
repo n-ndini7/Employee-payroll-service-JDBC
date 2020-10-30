1fb195b9bf882b828b551a9fb85af69d34bcba4d
n-ndini7 <hp@RJs-PC>
2020-10-30 12:23:12 +0530

    [Nandini] Added UC1 - connect to database

---
+++ src/main/java/com/capgemini/EmployeePayrollDBService/EmployeePayrollJDBCException.java
@@ -0,0 +1,16 @@
+package com.capgemini.EmployeePayrollDBService;
+
+//This class throws custom exceptions for employee payroll service JDBC program
+public class EmployeePayrollJDBCException extends Exception {
+
+	public enum ExceptionType {
+		WRONG_CREDENTIALS, CANNOT_LOAD_DRIVER;
+	}
+
+	ExceptionType type;
+
+	public EmployeePayrollJDBCException(String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+}
